baf9331d8511d39efda8a82ed65cb87e44d1452b
nbolanosbenavides <43621424+nbolanosbenavides@users.noreply.github.com>
2019-03-12 21:51:56 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPCIgQW2S36p8xL8zkMiAX4TjVtsYk/LZ+zA1zf59n+TU5jmLSMl46gpIeooiUJNqn71wxcScITqwCgU9R5QlbReZnhgHbEmk2s1BtXZrNFg/UfpwpFD47f2HbUMDIirISm8f3TbjnLBIUJ853x6+MvGKAH3KrBsiRvGZLlgA7kszcrhqrdtSXK+whv/iEbCscC0T7XLcUpp6RiY3vqJgMLKbnZdyfmyU/hTbV5oSRXFlxNEhE+uFoodJXZgZ908CElDOdsdNm8i6kQ+f3hVy3Y98wKVKkeJQ4Bo6wU9aHydL5PWUy/Xn/dAIJrYdQd95Q63r7SX07qSxn7chu/oNF',
 	}  
 }
