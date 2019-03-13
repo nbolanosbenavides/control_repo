74c5168bdf271e2e581aa9854d5b4471fe91c685
nbolanosbenavides <43621424+nbolanosbenavides@users.noreply.github.com>
2019-03-12 22:49:44 -0600

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,5 +1,5 @@
 class minecraft {
-  $url= 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
+  $url= 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar'
   $install_dir = '/opt/minecraft'
 }{
   file {$install_dir:
